922f821bf491986cb6e064547e65ba304686af75
raja vijay <rajavijay.gorla@outlook.com>
2019-05-20 07:49:25 +0530

    Revert "Revert "Revert "2 time"""
    This reverts commit 653a069264e131da690e35c67571309b0a55991b.

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -11,7 +11,6 @@ namespace ConsoleApp3
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World India");
-            Console.WriteLine("Hello World India");
         }
     }
 }
